d47aa35e7fb2a20f8e6c5648d4becfee469bb5ae
marcoavellaneda <mavellaneda81@gmail.com>
2023-08-03 21:39:09 -0300

    first commit

---
+++ src/main/java/com/mindhub/homebanking/models/Client.java
@@ -0,0 +1,67 @@
+package com.mindhub.homebanking.models;
+
+import org.hibernate.annotations.GenericGenerator;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+
+import javax.persistence.Id;
+
+
+@Entity
+public class Client {
+
+    //Atributos o propiedades
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO, generator = "native")
+    @GenericGenerator(name="native", strategy = "native")
+    private Long id;
+    private String name, lastName, mail;
+
+    //Constructores
+
+    public Client(){}
+    public Client(String name, String lastName, String mail){
+    this.name=name;
+    this.lastName=lastName;
+    this.mail=mail;
+}
+
+    //Métodos o comportamientos
+
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getMail() {
+        return mail;
+    }
+
+    public void setMail(String mail) {
+        this.mail = mail;
+    }
+
+    @Override
+    public String toString() {
+        return "Client{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", mail='" + mail + '\'' +
+                '}';
+    }
+}
